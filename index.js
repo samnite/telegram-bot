4ee7b1eca1dd514aea6e5cb124e0e0cbd345f1e5
Alexander Gribenchenko <samnite87@gmail.com>
2020-05-28 16:15:56 +0300

    disable badwords filter

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ const CronJob = require("cron").CronJob;
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-// bot.start((ctx) => ctx.reply("Welcome"));
-// bot.help((ctx) => ctx.reply("Send me a sticker"));
-// bot.on("sticker", (ctx) => ctx.reply("👍"));
-// bot.hears("hi", (ctx) => ctx.reply("Hey there"));
-
 // Cron task
 const job = new CronJob("0 0 */6 * * *", function () {
   // Update Covid-19 Database
