331f6ab6cd09ddeed5205055a9d404443b4f6baa
Viacheslav Krasnianskyi <viacheslav.krasnianskyi@crionis.com>
2020-08-24 13:31:52 +0300

    remove Covid update message

--- util/updateCovidBase.js
+++ util/updateCovidBase.js
@@ -6,15 +6,15 @@ const updateCovidData = (ctx) => {
     .get("https://api.covid19api.com/summary")
     .then((res) => {
       const date = new Date();
-
+      console.log("Base updated successfully");
       // Send Message
-      ctx.telegram
-        .sendMessage(
-          "-1001307324588",
-          `Covid-19 Database Successfully Updated : ${date}`
-        )
-        .then((info) => console.log(info))
-        .catch((err) => console.log(err));
+      // ctx.telegram
+      //   .sendMessage(
+      //     "-1001307324588",
+      //     `Covid-19 Database Successfully Updated : ${date}`
+      //   )
+      //   .then((info) => console.log(info))
+      //   .catch((err) => console.log(err));
       // Write file to server
       const data = JSON.stringify(res.data);
       fs.writeFile("./db/covidInfo.json", data, (i) => console.log(i));
