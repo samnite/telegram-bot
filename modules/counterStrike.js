ad8e6cbffabae101bf0c9774917a968c6275764e
Viacheslav Krasnianskyi <viacheslav.krasnianskyi@crionis.com>
2020-08-24 13:38:04 +0300

    remove cs info

--- modules/counterStrike.js
+++ modules/counterStrike.js
@@ -1,19 +1,7 @@
 const counterStrikeInfo = (ctx) => {
   const text =
-    "Counter-Strike Server IP: *104.199.71.150:27015*\n" +
-    "Half-Life Server IP: *34.77.193.114:27015*\n" +
-    "Команда коннекта из консоли: ` connect 34.77.193.114:27015;` \n\n" +
-    "*Полезные ссылки:*\n" +
-    "[Скачать клиент (Windows)](https://down-cs.su/clean.php)\n" +
-    "[Скачать клиент (Mac)](https://rutracker.org/forum/viewtopic.php?t=5239713)\n" +
-    "[Голосовая конференция](https://meet.jit.si/cs16)\n\n" +
-    "*Доступ к админке*:\n" +
-    "- меняем ник на *admin*\n" +
-    "- в консоли (меню ботов): ` amx_pbmenu`\n" +
-    "- в консоли (меню сервера): ` amxmodmenu`\n" +
-    "\n" +
-    "- Смена карты : ` amx_map MAP_NAME` \n" +
-    "- Серверные команды: ` amx_rcon COMAND` \n   (пример: `amx_rcon mp_roundtime 3`)";
+    "Counter-Strike Server IP: *CLOSED*\n" +
+    "Half-Life Server IP: *CLOSED*\n";
   return ctx.reply(text, {
     parse_mode: "markdown",
     disable_web_page_preview: true,
