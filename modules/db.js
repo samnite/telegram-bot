9ecd9df9dd16aa6cf6c7a9e5c537ace0a5eb4032
samnite <samnite87@gmail.com>
2020-10-02 08:48:18 +0300

    add mysql

---
+++ modules/db.js
@@ -0,0 +1,9 @@
+const mysql = require("mysql");
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const db = {
+  mysql_pool: mysql.createPool(process.env.CLEARDB_DATABASE_URL),
+};
+
+module.exports = db;
--- util/updateCovidBase.js
+++ util/updateCovidBase.js
@@ -1,32 +1,34 @@
 const axios = require("axios");
 const fs = require("fs");
+const db = require("../modules/db").mysql_pool;
 
-const updateCovidData = (ctx) => {
-  axios
+const updateCovidData = async (ctx) => {
+  const data = await axios
     .get("https://api.covid19api.com/summary")
     .then((res) => {
       const date = new Date();
-      console.log("Base updated successfully");
-      // Just placeholder to test commit
-      // Send Message
-      // ctx.telegram
-      //   .sendMessage(
-      //     "-1001307324588",
-      //     `Covid-19 Database Successfully Updated : ${date}`
-      //   )
-      //   .then((info) => console.log(info))
-      //   .catch((err) => console.log(err));
-      // Write file to server
-      const data = JSON.stringify(res.data);
-      fs.writeFile("./db/covidInfo.json", data, (i) => console.log(i));
+      console.log(new Date());
+
+      return res.data;
+      // const data = JSON.stringify(res.data);
+      // // console.log("Global DATA", res.data.Global);
+      // // console.log("Global DATA", res.data.Date);
+      // fs.writeFile("./db/covidInfo.json", data, (i) => console.log(i));
     })
     .catch((err) => {
       console.log(err);
-      ctx.telegram
-        .sendMessage("-1001307324588", `Schedule Update failed: ${err.message}`)
-        .then((info) => console.log(info))
-        .catch((err) => console.log(err));
+      // ctx.telegram
+      //   .sendMessage("-1001307324588", `Schedule Update failed: ${err.message}`)
+      //   .then((info) => console.log(info))
+      //   .catch((err) => console.log(err));
     });
+  console.log(data);
+  db.getConnection(function (err, connection) {
+    connection.query("SELECT * FROM covid_global", (err, res) => {
+      if (err) throw err;
+      console.log("Result", res[0]);
+    });
+  });
 };
 
 module.exports = { updateCovidData };
