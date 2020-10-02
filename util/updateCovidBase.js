9b5b158e483e80607b3976940ccbc918f7ca5488
samnite <samnite87@gmail.com>
2020-10-02 09:40:14 +0300

    add update global covid info endpoint

--- util/updateCovidBase.js
+++ util/updateCovidBase.js
@@ -1,34 +1,19 @@
 const axios = require("axios");
-const fs = require("fs");
 const db = require("../modules/db").mysql_pool;
 
 const updateCovidData = async (ctx) => {
   const data = await axios
     .get("https://api.covid19api.com/summary")
-    .then((res) => {
-      const date = new Date();
-      console.log(new Date());
-
-      return res.data;
-      // const data = JSON.stringify(res.data);
-      // // console.log("Global DATA", res.data.Global);
-      // // console.log("Global DATA", res.data.Date);
-      // fs.writeFile("./db/covidInfo.json", data, (i) => console.log(i));
-    })
-    .catch((err) => {
-      console.log(err);
-      // ctx.telegram
-      //   .sendMessage("-1001307324588", `Schedule Update failed: ${err.message}`)
-      //   .then((info) => console.log(info))
-      //   .catch((err) => console.log(err));
+    .then((res) => res.data)
+    .catch((err) => console.log(err));
+  if (data) {
+    db.getConnection(function (err, connection) {
+      connection.query(
+        `UPDATE covid_global SET new_confirmed = ${data.Global.NewConfirmed}, total_confirmed = ${data.Global.TotalConfirmed}, new_deaths = ${data.Global.NewDeaths}, total_deaths = ${data.Global.TotalDeaths}, new_recovered = ${data.Global.NewRecovered}, total_recovered = ${data.Global.TotalRecovered} WHERE id = 1`,
+        (err) => console.log(err)
+      );
     });
-  console.log(data);
-  db.getConnection(function (err, connection) {
-    connection.query("SELECT * FROM covid_global", (err, res) => {
-      if (err) throw err;
-      console.log("Result", res[0]);
-    });
-  });
+  }
 };
 
 module.exports = { updateCovidData };
