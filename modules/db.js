9bd1d97b2f3f89d5e8096a1d8815a456ad897098
samnite <samnite87@gmail.com>
2020-10-02 12:03:01 +0300

    add update covid info by countries endpoint

--- modules/db.js
+++ modules/db.js
@@ -3,7 +3,9 @@ const mysql = require("mysql");
 if (process.env.NODE_ENV !== "production") require("dotenv").config();
 
 const db = {
-  mysql_pool: mysql.createPool(process.env.CLEARDB_DATABASE_URL),
+	mysql_pool: mysql.createPool(process.env.CLEARDB_DATABASE_URL, {
+		multipleStatements: true,
+	}),
 };
 
 module.exports = db;
--- util/updateCovidBase.js
+++ util/updateCovidBase.js
@@ -1,19 +1,57 @@
 const axios = require("axios");
 const db = require("../modules/db").mysql_pool;
+const mysql = require("mysql");
 
 const updateCovidData = async (ctx) => {
-  const data = await axios
-    .get("https://api.covid19api.com/summary")
-    .then((res) => res.data)
-    .catch((err) => console.log(err));
-  if (data) {
-    db.getConnection(function (err, connection) {
-      connection.query(
-        `UPDATE covid_global SET new_confirmed = ${data.Global.NewConfirmed}, total_confirmed = ${data.Global.TotalConfirmed}, new_deaths = ${data.Global.NewDeaths}, total_deaths = ${data.Global.TotalDeaths}, new_recovered = ${data.Global.NewRecovered}, total_recovered = ${data.Global.TotalRecovered} WHERE id = 1`,
-        (err) => console.log(err)
-      );
-    });
-  }
+	let values = [];
+	// let updateQueries = "";
+	const data = await axios
+		.get("https://api.covid19api.com/summary")
+		.then((res) => res.data)
+		.catch((err) => console.log(err));
+	if (data) {
+		// Update global covid info
+		db.getConnection(function (err, connection) {
+			connection.query(
+				`UPDATE covid_global SET new_confirmed = ${data.Global.NewConfirmed}, total_confirmed = ${data.Global.TotalConfirmed}, new_deaths = ${data.Global.NewDeaths}, total_deaths = ${data.Global.TotalDeaths}, new_recovered = ${data.Global.NewRecovered}, total_recovered = ${data.Global.TotalRecovered} WHERE id = 1`,
+				(err) => console.log(err)
+			);
+		});
+		// Update covid info by country
+		data.Countries.forEach((el) => {
+			delete el.Date;
+			delete el.Premium;
+			values.push(Object.values(el));
+		});
+		// values.forEach((item) => {
+		// 	updateQueries += mysql.format(
+		// 		`UPDATE covid_countries SET country = ?, country_code = ?, slug = ?, new_confirmed = ?, total_confirmed = ?, new_deaths = ?, total_deaths = ?, new_recovered = ?, total_recovered = ? WHERE country_code = ?; `,
+		// 		[
+		// 			item.Country,
+		// 			item.CountryCode,
+		// 			item.Slug,
+		// 			item.NewConfirmed,
+		// 			item.TotalConfirmed,
+		// 			item.NewDeaths,
+		// 			item.TotalDeaths,
+		// 			item.NewRecovered,
+		// 			item.TotalRecovered,
+		// 			item.CountryCode,
+		// 		]
+		// 	);
+		// });
+		// db.getConnection(function (err, connection) {
+		// 	connection.query(updateQueries, (err) => console.log(err));
+		// });
+		db.getConnection(function (err, connection) {
+			connection.query("TRUNCATE covid_countries", (err) => console.log(err));
+			connection.query(
+				"INSERT INTO covid_countries (country, country_code, slug, new_confirmed, total_confirmed, new_deaths, total_deaths, new_recovered, total_recovered) VALUES ?",
+				[values],
+				(err) => console.log(err)
+			);
+		});
+	}
 };
 
 module.exports = { updateCovidData };
